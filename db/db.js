6113358c98b29a5548d0236377fe675926fac316
Keri Weiss <keret.weiss@gmail.com>
2021-03-05 15:46:35 -0500

    working on heroku

--- db/db.js
+++ db/db.js
@@ -1,6 +1,8 @@
 const pg = require('pg');
 const SQL = require('./seed');
-const client = new pg.Client('postgres://localhost/seating_chart');
+const client = new pg.Client(
+  process.env.DATABASE_URL || 'postgres://localhost/seating_chart'
+);
 
 const syncAndSeed = async () => {
   await client.query(SQL);
